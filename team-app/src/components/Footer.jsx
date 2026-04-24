3c98e1ee01a194582e95afab9c4bb60ea12c3ee5
zhangz3462 <zhangz3462@my.sduhsd.net>
2026-04-24 13:28:24 -0700

    update footer css

--- team-app/src/components/Footer.jsx
+++ team-app/src/components/Footer.jsx
@@ -1,13 +1,14 @@
-import "/src/App.css"
+import React from 'react';
+import Logo from './Logo.png';
+import '../App.css'; 
 
-
-export default function Footer() { 
-  return ( 
-    <div className="footer"> 
+export default function Footer() {
+  return (
+    <div className="footer">
       <footer className="footer-text">
         <span>&copy; Aiju Corp.</span>
         <span className="footer-right-text">All rights reserved by the Nauru Parliament.</span>
-      </footer> 
-    </div> 
-  ); 
+      </footer>
+    </div>
+  );
 }
