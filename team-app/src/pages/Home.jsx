5f4d81dc4e164af834b92e4cad5b0d8a54e6251a
zhangz3462 <zhangz3462@my.sduhsd.net>
2026-04-24 12:30:19 -0700

    Updated formatting on main.jsx

--- team-app/src/pages/Home.jsx
+++ team-app/src/pages/Home.jsx
@@ -5,10 +5,10 @@ export default function Home() {
       <p>This is a website for calculating grades, made for primarily Canyon Crest Academy Students.</p>
       <p>Our features include:</p>
       <ul>
-        <li>1. A grade point average calculator</li>
-        <li>2. Course selection profile, including recommendations</li>
-        <li>3. Credits for different classes, including weighted or unweighted classes</li>
-        <li>4. An area to input your own grades, allowing faster GPA calculation, and credit information!</li>
+        <li>A grade point average calculator</li>
+        <li>Course selection profile, including recommendations</li>
+        <li>Credits for different classes, including weighted or unweighted classes</li>
+        <li>An area to input your own grades, allowing faster GPA calculation, and credit information!</li>
       </ul>
     <p>To use this website, click on the navigation bar above. For better results, please log in to save your grades, though it is not required. The website will remain functional without logging in. All information is anonymous.</p>
     <p>Thanks for using our website!</p>
