7a686c9a9af4ead4f2ad7620c9fe35218fe1f604
zhangz3462 <zhangz3462@my.sduhsd.net>
2026-04-28 12:40:52 -0700

    added logo to Footer.jsx and changed App.css to make text white

--- team-app/src/components/Footer.jsx
+++ team-app/src/components/Footer.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-// import Logo from './Logo.png';
+import rzLogo from './rzLogo.png';
 import '../App.css'; 
 
 export default function Footer() {
   return (
     <div className="footer">
-      <footer className="footer-text">
+      <footer className="footer-content" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        
+        <div className="footer-left" 
+          style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+        <img src={rzLogo} alt="Company Logo" 
+          style={{ height: '3.3rem', width: '3.3rem'}} />
         <span>&copy; Aiju Corp.</span>
+        </div>
+
         <span className="footer-right-text">All rights reserved by the Nauru Parliament.</span>
       </footer>
     </div>
