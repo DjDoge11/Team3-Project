e7a7484e75671e15a95294a8395471549a755ec7
zhangz3462 <zhangz3462@my.sduhsd.net>
2026-05-04 13:08:38 -0700

    Revert "Merge branch 'main' into frontEnd/Kevin"
    This reverts commit 6ca9aa6fd58338293868c44bfa129743c2693e47, reversing
    changes made to 70f182fcd9cb994413fec2d771437472ccb28e4b.

--- team-app/src/components/Navbar.jsx
+++ team-app/src/components/Navbar.jsx
@@ -1,33 +1,16 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { auth } from '../firebase'; // Adjust path as needed
-import { onAuthStateChanged } from 'firebase/auth';
 
 export default function Navbar() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    });
-    return () => unsubscribe();
-  }, []);
-
   return (
-    <nav className="navbar">
+    <nav class = "navbar">
       <ul>
-      <li><Link to="/home">Home</Link></li>
+        <li><Link to="/home">Home</Link></li>
         <li><Link to="/gpa">GPA</Link></li>
         <li><Link to="/courses">Courses</Link></li>
         <li><Link to="/grades">Grades</Link></li>
         <li><Link to="/login">Login</Link></li>
       </ul>
-
-      <div className="username">
-          {user ? (
-          <p>{user.email}</p>
-        ) : (<p>Guest</p>)}
-      </div>
     </nav>
   );
 }
+
